d16c16c651b97916c5a3f1147bf779cc7d4898eb
haolongyang <hlyang0039@gmail.com>
2021-08-18 23:04:43 -0400

    Add Network Module and API

---
+++ app/src/main/java/com/example/food_recipes/MyApplication.kt
@@ -0,0 +1,8 @@
+package com.example.food_recipes
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class MyApplication: Application() {
+}
---
+++ app/src/main/java/com/example/food_recipes/util/Constants.kt
@@ -0,0 +1,10 @@
+package com.example.food_recipes.util
+
+class Constants {
+
+    companion object{
+
+        const val BASE_URL = "https://api.spoonacular.com"
+        const val API_KEY = "e1d5923a13af43aeb938bcea72a5f1b1"
+    }
+}
